3b1b7d5a51a56a77c715c17e5f25b984c23c9abe
Viljami Anttonen <viljami.anttonen@gmail.com>
2018-03-22 22:42:24 +0200

    Added everything

---
+++ README.md
@@ -0,0 +1,1 @@
+# EcHoAsCiI 😱
---
+++ bin/echoascii.js
@@ -0,0 +1,15 @@
+#! /usr/bind/env node
+if (process.argv.length <= 2) process.exit(-1)
+
+const figlet = require('figlet')
+const rcfile = require('rcfile')
+const config = rcfile('echoascii')
+
+const str = process.argv[2]
+const result = figlet.textSync(`\n ${str} \n`, {
+  font: config.font || 'Larry 3D',
+  horizontalLayout: 'default',
+  verticalLayout: 'default',
+})
+
+console.log(result)
